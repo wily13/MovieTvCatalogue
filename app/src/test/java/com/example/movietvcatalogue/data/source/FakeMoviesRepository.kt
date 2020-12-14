4b42f89dc55a5d801c556db3e3b66d9d46ebc869
Wily13 <wily.355@gmail.com>
2020-12-14 12:11:10 +0700

    MoviesRepository for repository test

--- app/src/test/java/com/example/movietvcatalogue/data/source/FakeMoviesRepository.kt
+++ app/src/test/java/com/example/movietvcatalogue/data/source/FakeMoviesRepository.kt
@@ -1,4 +1,101 @@
 package com.example.movietvcatalogue.data.source
 
-class FakeMoviesRepository {
+import com.example.movietvcatalogue.data.MoviesEntity
+import com.example.movietvcatalogue.data.source.remote.RemoteDataSource
+
+class FakeMoviesRepository (private val remoteDataSource: RemoteDataSource) : MoviesDataSource {
+
+    override fun getAllMovies(): List<MoviesEntity> {
+        val courseResponses = remoteDataSource.getDataMovies()
+        val moviesList = ArrayList<MoviesEntity>()
+        for (response in courseResponses) {
+            val movies = MoviesEntity(response.moviesId,
+                response.title,
+                response.genre,
+                response.year,
+                response.dateRelease,
+                response.description,
+                response.director,
+                response.creator,
+                response.type,
+                response.status,
+                response.language,
+                response.budget,
+                response.revenue,
+                response.image)
+            moviesList.add(movies)
+        }
+        return moviesList
+    }
+
+    override fun getAllTvShows(): List<MoviesEntity> {
+        val courseResponses = remoteDataSource.getDataTvShows()
+        val tvShowsList = ArrayList<MoviesEntity>()
+        for (response in courseResponses) {
+            val tvShows = MoviesEntity(response.moviesId,
+                response.title,
+                response.genre,
+                response.year,
+                response.dateRelease,
+                response.description,
+                response.director,
+                response.creator,
+                response.type,
+                response.status,
+                response.language,
+                response.budget,
+                response.revenue,
+                response.image)
+            tvShowsList.add(tvShows)
+        }
+        return tvShowsList
+    }
+
+    override fun getMoviesDetail(moviesId: String): MoviesEntity {
+        val courseResponses = remoteDataSource.getDataMovies()
+        lateinit var movies: MoviesEntity
+        for (response in courseResponses) {
+            if (response.moviesId == moviesId) {
+                movies = MoviesEntity(response.moviesId,
+                    response.title,
+                    response.genre,
+                    response.year,
+                    response.dateRelease,
+                    response.description,
+                    response.director,
+                    response.creator,
+                    response.type,
+                    response.status,
+                    response.language,
+                    response.budget,
+                    response.revenue,
+                    response.image)
+            }
+        }
+        return movies
+    }
+
+    override fun getTvShowsDetail(tvShowsId: String): MoviesEntity {
+        val courseResponses = remoteDataSource.getDataTvShows()
+        lateinit var tvShows: MoviesEntity
+        for (response in courseResponses) {
+            if (response.moviesId == tvShowsId) {
+                tvShows = MoviesEntity(response.moviesId,
+                    response.title,
+                    response.genre,
+                    response.year,
+                    response.dateRelease,
+                    response.description,
+                    response.director,
+                    response.creator,
+                    response.type,
+                    response.status,
+                    response.language,
+                    response.budget,
+                    response.revenue,
+                    response.image)
+            }
+        }
+        return tvShows
+    }
 }
--- app/src/test/java/com/example/movietvcatalogue/data/source/MoviesRepositoryTest.kt
+++ app/src/test/java/com/example/movietvcatalogue/data/source/MoviesRepositoryTest.kt
@@ -1,13 +1,28 @@
 package com.example.movietvcatalogue.data.source
 
+import com.example.movietvcatalogue.data.MoviesEntity
+import com.example.movietvcatalogue.data.source.remote.RemoteDataSource
+import com.example.movietvcatalogue.utils.MoviesDataDummy
+import com.google.common.truth.Truth
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 class MoviesRepositoryTest {
 
+    private val remote = Mockito.mock(RemoteDataSource::class.java)
+    private val academyRepository = FakeMoviesRepository(remote)
+
+    private val moviesResponses = MoviesDataDummy.generateRemoteDummyMovies()
+    private val moviesId = moviesResponses[0].moviesId
+    private val tvShowsResponses = MoviesDataDummy.generateRemoteDummyTvShows()
+    private val tvShowsId = tvShowsResponses[0].moviesId
+
     @Test
     fun getAllMovies() {
+
     }
 
     @Test
